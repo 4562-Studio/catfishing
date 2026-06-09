fe86114704f5bb61028ad4e88825d46bce392ccc
tj-dodd <teamphazium@hotmail.com>
2026-06-09 22:30:49 +1000

    Basic fishingareas command

---
+++ src/commands/fishingarea.ts
@@ -0,0 +1,93 @@
+import type { Command } from "./index";
+import {
+  ChannelType,
+  SlashCommandBuilder,
+  ThreadAutoArchiveDuration,
+  type TextChannel,
+} from "discord.js";
+
+type FishingArea = {
+  threadId: string;
+  threadName: string;
+  userId: string;
+};
+
+const activeFishingAreas = new Map<string, FishingArea>(); // Move from memory to DB at some point?
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("fishingarea")
+    .setDescription("Creates a temporary thread for you to fish in.")
+    .addStringOption((option) =>
+      option
+        .setName("name")
+        .setDescription("The thread's name. Format: (user)'s (name)")
+        .setRequired(true)
+        .setMinLength(1)
+        .setMaxLength(50),
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("private")
+        .setDescription("Should the thread be private?"),
+    ).toJSON(),
+
+  async execute(interaction) {
+    const botChannel = interaction.guild?.channels.cache.find(
+      (channel): channel is TextChannel =>
+        channel.name === "catfishing" &&
+        channel.type === ChannelType.GuildText,
+    );
+
+    const userFishingArea = activeFishingAreas.get(interaction.user.id);
+
+    if (botChannel == undefined || interaction.channelId !== botChannel.id) {
+      await interaction.reply({
+        content: "You can only use this command in the #catfishing channel!",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    if (userFishingArea != undefined) {
+      const existingThread = await interaction.client.channels
+        .fetch(userFishingArea.threadId)
+        .catch(() => null);
+
+      if (existingThread?.isThread() && !existingThread.archived) {
+        await interaction.reply({
+          content: `You already have a fishing channel called ${userFishingArea.threadName}!`,
+          ephemeral: true,
+        });
+        return;
+      }
+      activeFishingAreas.delete(interaction.user.id);
+    }
+
+    const threadName = interaction.options.getString("name", true);
+    const isPrivate = interaction.options.getBoolean("private") ?? false;
+
+    const thread = await botChannel.threads.create({
+      name: `${interaction.user.username}'s ${threadName}`,
+      reason: "Go fish!",
+      autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
+      type: isPrivate ? ChannelType.PrivateThread : ChannelType.PublicThread,
+    });
+
+    const fishingArea: FishingArea = {
+        threadId: thread.id,
+        threadName: thread.name,
+        userId: interaction.user.id,
+        };
+
+    activeFishingAreas.set(interaction.user.id, fishingArea);
+    
+    await thread.members.add(interaction.user.id);
+    await thread.send("Good luck!");
+
+    await interaction.reply({
+      content: `Created fishing area: ${thread}`,
+      ephemeral: true,
+    });
+  },
+} satisfies Command;
